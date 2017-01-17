293facd059b11dd6dee064a2b6e936faa5f76fb2
Leonardo Cintra <leonardo.ncintra@outlook.com>
2017-01-17 21:46:27 -0200

    ala global

---
+++ FundamentosMEAN/node/ex04_globlal.js
@@ -0,0 +1,4 @@
+const P1 = 3.14;
+console.log(global.PI);
+
+global.obj = {name: "Estou no global"};
---
+++ FundamentosMEAN/node/ex04_test.js
@@ -0,0 +1,4 @@
+require('./ex04_globlal')
+
+console.log(global.obj.name)
+console.log(obj.name)
