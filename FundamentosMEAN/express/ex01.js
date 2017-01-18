3da3d60b366634d32b05f7d2cee710daf2ff6b38
Leonardo Cintra <leonardo.ncintra@outlook.com>
2017-01-17 22:55:10 -0200

    executando no browser

--- FundamentosMEAN/express/ex01.js
+++ FundamentosMEAN/express/ex01.js
@@ -1,1 +1,8 @@
 const express = require('express')
+const server = express()
+
+server.get('/', function(req, res) {
+    res.send('<h1> Index! </h1>')
+})
+
+server.listen(3000, () => console.log('Executando...'))
