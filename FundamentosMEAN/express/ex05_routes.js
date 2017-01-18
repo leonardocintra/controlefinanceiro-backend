4e2deb6e255da25834b166938eef42cf2becf990
Leonardo Cintra <leonardo.ncintra@outlook.com>
2017-01-18 12:39:05 -0200

    passando parametros na rota

--- FundamentosMEAN/express/ex05_routes.js
+++ FundamentosMEAN/express/ex05_routes.js
@@ -2,5 +2,17 @@ const express = require('express')
 const router = express.Router()
 
 router.use((req, res, next) => {
-    
+    const init = Date.now()
+    next()
+    console.log(`Tempo = ${Date.now() - init} ms.`)
 })
+
+router.get('/produtos/:id', (req, res) => {
+    res.json({id: req.params.id, name: 'Ronaldo, briia muto nu curintia'})
+})
+
+router.get('/clientes/:id/:name', (req, res) => {
+    res.json({id: req.params.id, name: req.params.name })
+})
+
+module.exports = router
