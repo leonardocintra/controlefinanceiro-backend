49d09148dbd85248a184c4981db58e00d88bf27d
Leonardo Cintra <leonardo.ncintra@outlook.com>
2017-01-19 21:56:20 -0200

    tratando erros e traduzindo mesnagems

--- api/billingCycle/billingCycleService.js
+++ api/billingCycle/billingCycleService.js
@@ -1,5 +1,11 @@
 const BillingCycle = require('./billingCycle')
 
 BillingCycle.methods(['get', 'post', 'put', 'delete'])
+/*
+ new: true -> Apos dar um "PUT" ele retorna o objeto antigo novamente.
+              com esse "true" ele pede para mostrar o novo objeto atualizado
+ runValidators: true -> por padrão ele nao valida as regras (required, max, min) no PUT
+*/
+BillingCycle.updateOptions({new: true, runValidators: true})
 
 module.exports = BillingCycle
--- config/database.js
+++ config/database.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose')
 module.exports = mongoose.connect('mongodb://localhost/db_finance')
 
+// Tradução dos erros de forma GLOBAL
 mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório."
+mongoose.Error.messages.Number.min = "O '{VALUE}' informado é menor que o limite mínimo permitido de '{MIN}.'"
+mongoose.Error.messages.Number.max = "O '{VALUE}' informado é maior que o limite máximo permitido de '{MAX}.'"
+mongoose.Error.messages.String.enum = "O '{VALUE}' não é valido para o atributo '{PATH}'"
