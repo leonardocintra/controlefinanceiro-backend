248a006c1d9d146d53a95977659c15a3ac4860df
Leonardo Cintra <leonardo.ncintra@outlook.com>
2017-01-19 20:04:45 -0200

    montando o servidor

--- loader.js
+++ loader.js
@@ -1,1 +1,16 @@
 const port = 3003
+
+const bodyParser = require('body-parser')
+const express = require('express')
+const server = express()
+
+server.use(bodyParser.urlencoded({ extended: true }))
+server.use(bodyParser.json())
+
+server.listen(port, function() {
+    console.log(`API Controle Financeiro running on port ${port}.`)
+})
+
+server.use(function(req, res, next) {
+    res.send('Funcionou')
+})
