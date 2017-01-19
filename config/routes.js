354276f81ebe913e27fecb09e5a91208a51a415c
Leonardo Cintra <leonardo.ncintra@outlook.com>
2017-01-19 20:49:17 -0200

    mapeando o billingCycle

--- config/routes.js
+++ config/routes.js
@@ -4,7 +4,11 @@ const express = require('express')
 */
 module.exports = function(server) {
     
-    // API Routes
+    // API Routes - Somente funciona se tiver o /api/
     const router = express.Router()
     server.use('/api', router)
+
+    // rotas da API
+    const billingCycleService = require('../api/billingCycle/billingCycleService')
+    billingCycleService.register(router, '/billingCycles')
 }
