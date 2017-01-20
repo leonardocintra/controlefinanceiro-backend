f9b483a668f263827fec92589ce2ff35c59f33ad
Leonardo Cintra <leonardo.ncintra@outlook.com>
2017-01-20 13:26:26 -0200

    melhorando resposta de erro

--- api/billingCycle/billingCycleService.js
+++ api/billingCycle/billingCycleService.js
@@ -1,3 +1,4 @@
+const _ = require('lodash')
 const BillingCycle = require('./billingCycle')
 
 BillingCycle.methods(['get', 'post', 'put', 'delete'])
@@ -8,6 +9,26 @@ BillingCycle.methods(['get', 'post', 'put', 'delete'])
  runValidators: true -> por padrão ele nao valida as regras (required, max, min) no PUT
 */
 BillingCycle.updateOptions({new: true, runValidators: true})
+
+BillingCycle.after('post', sendErrorsOrNext).after('put', sendErrorsOrNext)
+
+function sendErrorsOrNext(req, res, next) {
+    const bundle = res.locals.bundle
+
+    if (bundle.errors) {
+        
+        var errors = parseErrors(bundle.errors)
+        res.status(500).json({errors})
+    } else {
+        next()
+    }
+}
+
+function parseErrors(nodeResfulErrors) {
+    const errors = []
+    _.forIn(nodeResfulErrors, error => errors.push(error.message))
+    return errors
+}
 
 BillingCycle.route('count', function(req, res, next) {
     BillingCycle.count(function(error, value) {
