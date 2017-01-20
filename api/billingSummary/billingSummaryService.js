f01e203be6de29fcb49ac220edc4a69a3d8b2411
Leonardo Cintra <leonardo.ncintra@outlook.com>
2017-01-20 12:43:43 -0200

    corrigindo de credits para credt

--- api/billingSummary/billingSummaryService.js
+++ api/billingSummary/billingSummaryService.js
@@ -9,7 +9,7 @@ function getSummary(req, res) {
     BillingCycle.aggregate({
         $project: {credit: { $sum: "$credits.value"}, debt: {$sum: "$debts.value"}}
     }, {
-        $group: {_id: null, credit: {$sum: "$credits"}, debt: {$sum: "$debt"} }
+        $group: {_id: null, credit: {$sum: "$credit"}, debt: {$sum: "$debt"} }
     }, {
         $project: {_id: 0, credit: 1, debt: 1}
     }, function(error, result) {
