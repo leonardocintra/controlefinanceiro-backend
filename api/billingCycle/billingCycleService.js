bddea539019856c7a479f08d93e15de529f6ec80
Leonardo Cintra <leonardo.ncintra@outlook.com>
2017-01-19 23:10:20 -0200

    implementado rota que retorna a quantidade

--- api/billingCycle/billingCycleService.js
+++ api/billingCycle/billingCycleService.js
@@ -1,11 +1,24 @@
 const BillingCycle = require('./billingCycle')
 
 BillingCycle.methods(['get', 'post', 'put', 'delete'])
+
 /*
  new: true -> Apos dar um "PUT" ele retorna o objeto antigo novamente.
               com esse "true" ele pede para mostrar o novo objeto atualizado
  runValidators: true -> por padrão ele nao valida as regras (required, max, min) no PUT
 */
 BillingCycle.updateOptions({new: true, runValidators: true})
+
+BillingCycle.route('count', function(req, res, next) {
+    BillingCycle.count(function(error, value) {
+        if (error) {
+            res.status(500).json({ errors: [error]})
+        }
+        else {
+            res.json({ value })
+        }
+    })
+})
+
 
 module.exports = BillingCycle
